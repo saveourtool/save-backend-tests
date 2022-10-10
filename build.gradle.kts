6ed5d18f6a8e90ec91e222da769cf7f8acedea86
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2022-10-10 20:07:24 +0300

    Build using the released SAVE version (0.3.0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 internal val coroutinesVersion = "1.6.4"
 internal val ktorVersion = "2.1.1"
-internal val saveVersion = "0.3.0-SNAPSHOT"
+internal val saveVersion = "0.3.0"
 internal val junitVersion = "5.9.0"
 internal val assertjVersion = "3.23.1"
 
