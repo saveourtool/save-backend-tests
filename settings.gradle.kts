1c57e8c7661f45d4aca008703c5e0f13fd843f9a
Andrey Shcheglov <andrewbass+saveourtool@gmail.com>
2022-11-29 17:38:44 +0300

    Enable the Gradle Enterprise plug-in for GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("com.gradle.enterprise") version "3.9"
+}
+
+gradleEnterprise {
+    if (System.getenv("CI") != null) {
+        buildScan {
+            publishAlways()
+            termsOfServiceUrl = "https://gradle.com/terms-of-service"
+            termsOfServiceAgree = "yes"
+        }
+    }
+}
